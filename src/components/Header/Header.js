10d3fc15ee57882f27bfe45be4826039adfbfddf
Илья Маликов <ilyakor.mostovoy@yandex.ru>
2019-04-10 20:06:06 +0300

    changed name table

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => (
   <header className="header">
-    <h1 className="header__title">Таблица тренировок</h1>
+    <h1 className="header__title">Таблица упражнений</h1>
   </header>
 );
 
